0ca7d0fd30190459178e86f4fd21c9907265013d
varunshukla <varunshukla415@gmail.com>
2020-09-08 19:55:25 +0530

    First commit for app

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import {WebView} from 'react-native-webview';
+
+const App = () => {
+  return (
+    <>
+      {/* <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.container}> */}
+        <WebView
+          source={{
+            uri: 'http://dp.eightbitguy.in',
+          }}
+        />
+      {/* </SafeAreaView> */}
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  spinnerTextStyle: {
+    color: '#FFF',
+  },
+  container: {
+    flex: 1,
+  },
+});
+export default App;
